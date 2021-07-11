8b357425fe5b72a98b3d731373add2e525838d99
Danny <danysaputra3003@gmail.com>
2021-07-11 17:21:12 +0700

    change slider

--- resources/app.js
+++ resources/app.js
@@ -1,14 +1,33 @@
-var myIndex = 0;
-autoSlide();
+const carousel = document.querySelector('.carousel-items').children;
+const nextBtn = document.querySelector('#nextBtn');
+const prevBtn = document.querySelector('#prevBtn');
+const totalCarousel = carousel.length;
+let index = 0;
 
-function autoSlide() {
-    var i;
-    var x = document.getElementsByClassName('carousel-image');
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = 'none';
+nextBtn.addEventListener('click', ()=> {
+    next('next');
+});
+
+prevBtn.addEventListener('click', ()=> {
+    next('prev');
+})
+
+function next(direction) {
+    if (direction == 'next') {
+        index++;
+        if (index == totalCarousel) {
+            index = 0;
+        };
+    } else {
+        if (index == 0) {
+            index = totalCarousel - 1;
+        } else {
+            index --;
+        }
     }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 2000);
+    
+    for (let i = 0; i < carousel.length; i++) {
+        carousel[i].classList.remove('active');
+    }
+    carousel[index].classList.add('active');
 }
